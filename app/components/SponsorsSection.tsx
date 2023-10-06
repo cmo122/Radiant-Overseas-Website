b823baa37b60825b56cc70a5d819b854971c6d7f
Curtis Mo <cmo122@users.noreply.github.com>
2023-10-05 18:30:31 -0700

    Fix sponsors carousel spacing issues

--- app/components/SponsorsSection.tsx
+++ app/components/SponsorsSection.tsx
@@ -1,30 +1,33 @@
 import React from 'react'
 import { SponsorsCard } from './SponsorsCard'
-import { Text, SimpleGrid, Image, Center } from '@mantine/core'
+import { Text, SimpleGrid, Image,  Group } from '@mantine/core'
 import { carrierDetailsList } from './Carriers List/carrierDetailsList'
 import { Carousel } from '@mantine/carousel';
-import '../css/SponsorsCarousel.module.css'
+import classes from '../css/SponsorsCarousel.module.css'
 
 export default function SponsorsSection(){
 
     const mobileSlides=carrierDetailsList.map((details)=>(
-        <Carousel.Slide  key={details.name} style={{justifyContent:"center", alignItems:"center"}}>
-            <Image src={details.logo.src} alt={details.name} w="350" h="220"/>
+        <Carousel.Slide  key={details.name} miw="100%" mih="100%">
+            <Image src={details.logo.src} alt={details.name} ml="md" mr="md" maw="80%" mah="80%"/>
         </Carousel.Slide>
     ))
 
 
     return(
-            <Center mb="lg" styles={{root:{display:"flex", flexDirection:"column"}}}>
+            <Group mb="lg" styles={{root:{display:"flex", flexDirection:"column"}}}>
                 <Text p="xl" ta="center" fz="36px" fw="900">Trusted by the largest international carriers</Text>
                 <SimpleGrid spacing="lg" ta="center" cols={6} visibleFrom='lg'>
                     {carrierDetailsList.map((details, index)=>(
                         <SponsorsCard details={details} key={index}/>
                     ))}
                 </SimpleGrid>
-                <Carousel withIndicators hiddenFrom='lg' w="500" h="250" ta="center" display={"flex"}>
+                <Carousel withIndicators hiddenFrom='lg'
+                slideGap="md" 
+                loop
+                classNames={classes}>
                     {mobileSlides}               
                 </Carousel>
-            </Center>
+            </Group>
     )
 }
