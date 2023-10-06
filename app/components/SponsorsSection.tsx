36e9a6bbc029f7bce45eb3f2cf8106626e053b10
Curtis Mo <cmo122@users.noreply.github.com>
2023-10-05 19:49:02 -0700

    Resolve build issues

--- app/components/SponsorsSection.tsx
+++ app/components/SponsorsSection.tsx
@@ -1,18 +1,20 @@
-import React from 'react'
+import React, {useRef} from 'react'
 import { SponsorsCard } from './SponsorsCard'
 import { Text, SimpleGrid, Image,  Group } from '@mantine/core'
 import { carrierDetailsList } from './Carriers List/carrierDetailsList'
 import { Carousel } from '@mantine/carousel';
+import Autoplay from 'embla-carousel-autoplay';
 import classes from '../css/SponsorsCarousel.module.css'
 
 export default function SponsorsSection(){
 
     const mobileSlides=carrierDetailsList.map((details)=>(
-        <Carousel.Slide  key={details.name} miw="100%" mih="100%">
+        <Carousel.Slide  key={details.name} >
             <Image src={details.logo.src} alt={details.name} ml="md" mr="md" maw="80%" mah="80%"/>
         </Carousel.Slide>
     ))
 
+    const autoplay = useRef(Autoplay({ delay: 2000 }));
 
     return(
             <Group mb="lg" styles={{root:{display:"flex", flexDirection:"column"}}}>
@@ -25,7 +27,9 @@ export default function SponsorsSection(){
                 <Carousel withIndicators hiddenFrom='lg'
                 slideGap="md" 
                 loop
-                classNames={classes}>
+                classNames={classes}
+                maw="95%"
+                plugins={[autoplay.current]}>
                     {mobileSlides}               
                 </Carousel>
             </Group>
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,11 +2,18 @@ import React from "react";
 import App from "./app";
 import { MantineProvider} from '@mantine/core';
 import '@mantine/carousel/styles.css';
+import Head from 'next/head';
+
 
 export default function Index(){
 
     return(
-        <MantineProvider defaultColorScheme="auto">
+        <MantineProvider defaultColorScheme="auto" >
+            <Head>
+            <title>ROE LINE</title>
+            <meta name="description" content="ROE LINE landing page" />
+            <meta name="viewport" content="width=1060px, maximum-scale=1.0" />
+            </Head>
             <App/>
         </MantineProvider>
     )
