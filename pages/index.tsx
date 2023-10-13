de239964a27ed921172f96fe2f3382368476beef
Curtis Mo <cmo122@users.noreply.github.com>
2023-10-12 23:02:33 -0700

    Update company name

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,8 +10,8 @@ export default function Index(){
     return(
         <MantineProvider defaultColorScheme="auto">
             <Head>
-            <title>ROE LINE</title>
-            <meta name="description" content="ROE LINE Freight Forwarding" />
+            <title>R.O.E. LINE</title>
+            <meta name="description" content="R.O.E. LINE Freight Forwarding" />
             <meta name="next-head-count" content="fill"/>
             <link rel="icon" href="/favicon.ico" />
             <html lang="en" />
